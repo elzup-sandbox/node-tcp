27c26a554465943a9e749cdfe7b526d31d3fd096
elzup <guild0105@gmail.com>
2021-05-07 10:28:33 +0900

    fix client

--- client.js
+++ client.js
@@ -1,9 +1,9 @@
 const net = require('net')
+const host = process.env.SERVER_HOST
 
-const host = '160.16.94.225'
-const client = net.connect('3001', 'host', () => {
+const client = net.connect('3001', host, () => {
   console.log('connected to server')
-  client.write('hiro:test')
+  client.write('hiro:HeHHHHHHHH')
 })
 
 client.on('data', (data) => {
